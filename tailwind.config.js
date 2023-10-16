c2411625731d3b67d94d7d7390cb6f4b0be7c36c
TRAN CONG DUC <trancongduc1107@gmail.com>
2023-10-16 16:54:39 +0700

    upload code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['ClashDisplay-Rugular', ...defaultTheme.fontFamily.sans]
+      },
+      colors:{
+        tomato:"#E50814",
+        marigold:"#ffbe0b",
+      }
+    },
   },
   plugins: [],
 }
