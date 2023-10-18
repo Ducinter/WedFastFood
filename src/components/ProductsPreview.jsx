7a24386b6005e09d4504de97ac001b7aa7ab0d48
TRAN CONG DUC <trancongduc1107@gmail.com>
2023-10-18 17:05:30 +0700

    upload code 1:42:09

--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -1,37 +1,38 @@
 import React, { useState, useEffect } from "react";
 
 // import 'react-multi-carousel/lib/styles.css';
-// import { ProductPreviewCard } from "./ProductPreviewCard";
-// import Carousel from 'react-multi-carousel';
-// import 'react-multi-carousel/lib/styles.css';
-// import { useDispatch } from "react-redux";
-// import { addToCart } from "../stores/cart/cartSlice";
+import { ProductPreviewCard } from "./ProductPreviewCard";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { useDispatch } from "react-redux";
+import { addToCart } from "../stores/cart/cartSlice";
+// import { AddProduct } from "./AddProduct";
 
 
 
 export const ProductsPreview = () => {
     const [products, setProducts] = useState([]);
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-    // const responsive = {
-    //     superLargeDesktop: {
-    //       // the naming can be any, depends on you.
-    //       breakpoint: { max: 4000, min: 3000 },
-    //       items: 5
-    //     },
-    //     desktop: {
-    //       breakpoint: { max: 3000, min: 1024 },
-    //       items: 3
-    //     },
-    //     tablet: {
-    //       breakpoint: { max: 1024, min: 464 },
-    //       items: 2
-    //     },
-    //     mobile: {
-    //       breakpoint: { max: 464, min: 0 },
-    //       items: 1
-    //     }
-    //   };
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
 
     useEffect(() => {
         fetch('http://localhost:8080/api/products')
@@ -40,24 +41,23 @@ export const ProductsPreview = () => {
             .catch(e => console.log(e))
     }, [])
 
-    // const onAddProduct = (product) => {
-    //     dispatch(addToCart(product))
-    // }
+    const onAddProduct = (product) => {
+        dispatch(addToCart(product))
+    }
     
     return (
-        <div className="container mx-auto pb-4 w-2/3 text-white">
-            {/* <Carousel responsive={responsive}> */}
-            <h2>Product</h2>
+        <div className="container mx-auto pb-4 w-2/3 text-white bg-black">
+            <Carousel responsive={responsive}>
             {
                 products.length > 0 && products.map((product, index) => {
                     return (
-                        <div key = {index}>{product.name}</div>
-                            
-                            
+                        <div className="w-full p-3">
+                            <ProductPreviewCard key={index} product={product} onAddProduct ={onAddProduct} />
+                        </div>
                     )
                 })
             }
-            {/* </Carousel> */}
+            </Carousel>
         </div>
     )
 }
